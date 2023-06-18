db8c471f35f1f858a8402c64b0c3c96dad834f7a
Atharva Mali <atharva.mali3081@gmail.com>
2023-06-18 22:29:57 +0530

    initial commit

---
+++ first-java-program/FirstIDEAProgram/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String name;
+        Scanner s= new Scanner(System.in);
+        System.out.print("Enter Name : ");
+        name=s.nextLine();
+        System.out.println("Welcome "+name);
+
+    }
+}
