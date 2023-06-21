8bfddf4a1c40df6606c52a19dfee2346d92819c3
Atharva Mali <atharva.mali3081@gmail.com>
2023-06-21 12:50:19 +0530

    Printing Hello World program in IDEA

--- programs/05-first-java-program/first-idea-program/first-idea-program/src/com/atharva/Main.java
+++ programs/05-first-java-program/first-idea-program/first-idea-program/src/com/atharva/Main.java
@@ -3,6 +3,7 @@ package com.atharva;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.print(34567);
+        System.out.println("My name is Atharva");
     }
 }
