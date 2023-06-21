d0d9a93db94ab24231c9d0f3b838f10eca78ea93
Atharva Mali <atharva.mali3081@gmail.com>
2023-06-21 16:16:49 +0530

    Update code for assignment 1

---
+++ assignments/02-first-java/first-java-assignment/src/com/atharva/LargestNumber.java
@@ -0,0 +1,23 @@
+package com.atharva;
+
+import java.util.Scanner;
+
+public class LargestNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number : ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number : ");
+        int num2 = sc.nextInt();
+
+        if (num1 > num2) {
+            System.out.println(num1 + " is largest number.");
+        } else {
+            System.out.println(num2 + " is largest number.");
+        }
+    }
+
+}
