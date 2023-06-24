c927e53de7289a59e03d804e50e259858964bbe2
Atharva Mali <atharva.mali3081@gmail.com>
2023-06-24 15:44:27 +0530

    Added code for Method Overloading in java lecture 7

--- programs/07-methods/code/src/com/atharva/VarArgs.java
+++ programs/07-methods/code/src/com/atharva/VarArgs.java
@@ -6,7 +6,16 @@ public class VarArgs {
     public static void main(String[] args) {
 //        fun(2, 3, 4, 56, 87, 23, 45, 65);
 //        fun();
-        multiple(2, 3, "Atharva", "Rahul", "jhfjhdkk");
+//        multiple(2, 3, "Atharva", "Rahul", "jhfjhdkk");
+//        demo();
+    }
+
+    static void demo(int... v) {
+        System.out.println(Arrays.toString(v));
+    }
+
+    static void demo(String... v) {
+        System.out.println(Arrays.toString(v));
     }
 
     static void multiple(int a, int b, String... v) {
